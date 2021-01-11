88c33a678a436da7e6be57ee9be8ada39e22c5ae
Clint Losee <clint.losee@gmail.com>
2021-01-11 10:40:05 -0700

    Separated out url list

--- backend/lib/scraper.js
+++ backend/lib/scraper.js
@@ -2,22 +2,9 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import db from './db';
 import { sendEmail, uniqueScrapes } from './utils';
+import urls from '../data/urls';
 
 require('dotenv').config();
-
-const urls = [
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/fiocchi-training-dynamics-300-aac-blackout-150gr-fmjbt-rifle-ammo-50-rounds/p/1656667',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/sellier-bellot-tactical-300-aac-blackout-200gr-fmj-rifle-ammo-20-rounds/p/1501421',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/sellier-bellot-tactical-300-aac-blackout-147gr-fmj-rifle-ammo-20-rounds/p/1501420',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/ppu-standard-rifle-300-aac-blackout-125gr-fmj-rifle-ammo-20-rounds/p/1671581',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/armscor-300-aac-blackout-147gr-fmj-rifle-ammo-20-rounds/p/1650006',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/sig-sauer-elite-performance-300-aac-blackout-125gr-fmj-rifle-ammo-20-rounds/p/1653080',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/ppu-standard-rifle-300-aac-blackout-125gr-hpbt-rifle-ammo-20-rounds/p/1671582',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/ammunition-ammo-for-hunting-shooting-sports/rifle-ammo-hunting-shooting-sports/federal-american-eagle-300-aac-blackout-150gr-fmjbt-rifle-ammo-20-rounds/p/1565033',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/gun-cases-locks/rifle-cases/gunmate-deluxe-40in-rifle-case-black/p/1215322',
-  'https://www.sportsmans.com/shooting-gear-gun-supplies/clay-sports/white-flyer-biodegradable-clay-targets-135-count/p/315699',
-];
-//* last 2 product are in stock
 
 function emailBodySetup(product) {
   return `<div>
