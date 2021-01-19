f048fbf81a97b2a366408a02a1c8569e864eec5c
Clint Losee <clint.losee@gmail.com>
2021-01-19 15:12:29 -0700

    URLs added. Updated project name. Added example cron times.

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -2,9 +2,9 @@ import cron from 'node-cron';
 import { runCron } from './scraper';
 
 //* Cron schedules
-const everyMinute = '* * * * *';
-const everyThirtyMinutes = '0,30 * * * *';
-const everyThirtyBetweenFiveAndTen = '*/30 05-22 * * *';
+// const everyMinute = '* * * * *';
+// const everyThirtyMinutes = '0,30 * * * *';
+// const everyThirtyBetweenFiveAndTen = '*/30 05-22 * * *';
 const onePastHourBetweenFiveAndTen = '01 05-22 * * *';
 
 //* To run every minute
